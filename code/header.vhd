e351ef08643499d925a250f2f9681b44bc09b8b3
Christian Gwerder <chris.gwerder@gmail.com>
2013-02-07 17:24:21 +0100

    Komplett überarbeitete DT1-Zusammenfassung von Christian Gwerder/Sven Körner
    - Basierend auf DT1!Zusammenfassung von Gian Claudio und Damian Köppel
    - Überlasse das Mergen gerne der nächsten Generation;-)

--- code/header.vhd
+++ code/header.vhd
@@ -1,7 +1,9 @@
 LIBRARY library_name_1 {, libary_name_2; ...};
-USE library_name.element_name;
+-- Mit USE wird Bibliotheksinhalt im ganzen VHDL Modul sichtbar
+USE library_name.element_name;	-- Nur element_name wird sichtbar
 --oder
-USE library_name.ALL
+USE library_name.ALL			-- Alle Elemente werden sichtbar
+
 --Beispiel
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
